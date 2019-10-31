66f1b76a322bc1ae16eaae1f692720653930608b
Brandon Shar <brandon.shar@icloud.com>
2019-10-31 19:05:46 -0400

    fix share persisting bug

--- lib/inertia_rails/renderer.rb
+++ lib/inertia_rails/renderer.rb
@@ -21,7 +21,7 @@ module InertiaRails
         @render_method.call json: page, status: 200
       else
         @render_method.call template: 'inertia', layout: ::InertiaRails.layout, locals: (view_data).merge({page: page})
-      end
+      end.tap { ::InertiaRails.reset! }
     end
 
     private
