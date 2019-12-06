6014396b06c07d625690ba3c092da311e638fa70
Brian Knoles <bknoles@gmail.com>
2019-12-06 17:23:38 -0600

    Wrap inertia_share calls in a before_action so that they recalculate on every request instead of just the first time that the controller file is loaded (#19)

--- spec/inertia/sharing_spec.rb
+++ spec/inertia/sharing_spec.rb
@@ -1,19 +1,40 @@
 RSpec.describe 'using inertia share when rendering views', type: :request do
   subject { JSON.parse(response.body)['props'].symbolize_keys }
-  
+
   context 'using inertia share' do
     let(:props) { {name: 'Brandon', sport: 'hockey', position: 'center', number: 29} }
     before { get share_path, headers: {'X-Inertia' => true} }
-    
+
     it { is_expected.to eq props }
   end
 
-  context 'inertia share across requests' do 
+  context 'inertia share across requests' do
     before do
-      get share_path, headers: {'X-Inertia' => true} 
+      get share_path, headers: {'X-Inertia' => true}
       get empty_test_path, headers: {'X-Inertia' => true}
     end
-  
+
     it { is_expected.to eq({}) }
+  end
+
+  context 'using inertia share in subsequent requests' do
+    let(:props) { {name: 'Brandon', sport: 'hockey', position: 'center', number: 29} }
+
+    before do
+      get share_path, headers: {'X-Inertia' => true}
+      get share_path, headers: {'X-Inertia' => true}
+    end
+
+    it { is_expected.to eq props }
+  end
+
+  context 'using inertia share with inheritance' do
+    let(:props) { {name: 'No Longer Brandon', sport: 'hockey', position: 'center', number: 29} }
+
+    before do
+      get share_with_inherited_path, headers: {'X-Inertia' => true}
+    end
+
+    it { is_expected.to eq props }
   end
 end
