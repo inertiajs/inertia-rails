68080dfa741ed639bbcd67ef56cd42f740dceb8b
Georg Ledermann <mail@georg-ledermann.de>
2019-12-12 11:26:06 +0100

    Add patch for gem `better_errors`, too

--- lib/inertia_rails.rb
+++ lib/inertia_rails.rb
@@ -2,6 +2,7 @@ require 'inertia_rails/renderer'
 require 'inertia_rails/engine'
 
 require 'patches/debug_exceptions'
+require 'patches/better_errors'
 
 ActionController::Renderers.add :inertia do |component, options|
   InertiaRails::Renderer.new(
