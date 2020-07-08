881aa09496ca173122ab0da8cedef9697c347e4e
Brian Knoles <bknoles@gmail.com>
2020-07-07 19:09:06 -0500

    Reorganize intertia data attributes to make the threadsafe act like private variables that have default values

--- lib/inertia_rails/inertia_rails.rb
+++ lib/inertia_rails/inertia_rails.rb
@@ -1,6 +1,6 @@
 module InertiaRails
-  thread_mattr_accessor :shared_plain_data
-  thread_mattr_accessor :shared_blocks
+  thread_mattr_accessor :threadsafe_shared_plain_data
+  thread_mattr_accessor :threadsafe_shared_blocks
 
   def self.configure
     yield(Configuration)
@@ -8,7 +8,7 @@ module InertiaRails
 
   # "Getters"
   def self.shared_data(controller)
-    threadsafe_shared_plain_data.merge!(evaluated_blocks(controller, threadsafe_shared_blocks))
+    shared_plain_data.merge!(evaluated_blocks(controller, shared_blocks))
   end
 
   def self.version
@@ -21,11 +21,11 @@ module InertiaRails
 
   # "Setters"
   def self.share(**args)
-    self.shared_plain_data = threadsafe_shared_plain_data.merge(args)
+    self.shared_plain_data = self.shared_plain_data.merge(args)
   end
 
   def self.share_block(block)
-    self.shared_blocks = threadsafe_shared_blocks + [block]
+    self.shared_blocks = self.shared_blocks + [block]
   end
 
   def self.reset!
@@ -44,20 +44,24 @@ module InertiaRails
     end
   end
 
+  # Getters and setters to provide default values for the threadsafe attributes
+  def self.shared_plain_data
+    self.threadsafe_shared_plain_data || {}
+  end
+
+  def self.shared_plain_data=(val)
+    self.threadsafe_shared_plain_data = val
+  end
+
+  def self.shared_blocks
+    self.threadsafe_shared_blocks || []
+  end
+
+  def self.shared_blocks=(val)
+    self.threadsafe_shared_blocks = val
+  end
+
   def self.evaluated_blocks(controller,  blocks)
     blocks.flat_map { |block| controller.instance_exec(&block) }.reduce(&:merge) || {}
-  end
-
-  # thread_mattr_accessor doesn't accept :default as an option, and since the method
-  # is actually defined in the context of Thread.current, we can't do something like:
-  # def self.shared_plain_data
-  #   @@shared_plain_data || {}
-  # end
-  def self.threadsafe_shared_plain_data
-    self.shared_plain_data || {}
-  end
-
-  def self.threadsafe_shared_blocks
-    self.shared_blocks || []
   end
 end
