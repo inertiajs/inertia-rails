0e97a4e6f657f7e6d87643f528979101264b5516
Brandon Shar <brandonshar@Brandons-MacBook-Pro.local>
2022-05-09 16:05:16 -0400

    test ssr conditions

--- spec/inertia/ssr_spec.rb
+++ spec/inertia/ssr_spec.rb
@@ -9,8 +9,9 @@ RSpec.describe 'inertia ssr', type: :request do
       end
     end
 
-    it 'returns the result of the ssr call' do
-      allow(Net::HTTP).to receive(:post)
+    context 'with a successful ssr response' do
+      before do
+        allow(Net::HTTP).to receive(:post)
         .with(
           URI('ssr-url/render'),
           {
@@ -27,13 +28,45 @@ RSpec.describe 'inertia ssr', type: :request do
             head: ['<title>Title works</title>'],
           }.to_json
         ))
+      end
 
+      it 'returns the result of the ssr call' do
+        get props_path
 
-      get props_path
+        expect(response.body).to include('<title>Title works</title>')
+        expect(response.body).to include('<div>Test works</div>')
+        expect(response.headers['Content-Type']).to eq 'text/html; charset=utf-8'
+      end
 
+      it 'allows inertia to take over when inertia headers are passed' do
+        get props_path, headers: {'X-Inertia' => true, 'X-Inertia-Version' => '1.0'}
 
-      expect(response.body).to include('<title>Title works</title>')
-      expect(response.body).to include('<div>Test works</div>')
+        expect(response.headers['Vary']).to eq 'Accept'
+        expect(response.headers['Content-Type']).to eq 'application/json; charset=utf-8'
+      end
+    end
+
+    context 'the ssr server fails for some reason' do
+      before do
+        allow(Net::HTTP).to receive(:post)
+        .with(
+          URI('ssr-url/render'),
+          {
+            component: 'TestComponent',
+            props: {name: 'Brandon', sport: 'hockey'},
+            url: props_path,
+            version: '1.0',
+          }.to_json,
+          'Content-Type' => 'application/json'
+        )
+        .and_raise('uh oh')
+      end
+
+      it 'renders inertia without ssr as a fallback' do
+        get props_path
+
+        expect(response.body).to include '<div id="app" data-page="{&quot;component&quot;:&quot;TestComponent&quot;,&quot;props&quot;:{&quot;name&quot;:&quot;Brandon&quot;,&quot;sport&quot;:&quot;hockey&quot;},&quot;url&quot;:&quot;/props&quot;,&quot;version&quot;:&quot;1.0&quot;}"></div>'
+      end
     end
   end
 end
