7b62df0923ba7c92ce3a90009e75cd39328422d2
Li (Boris) Ding <ding@brickdoc.com>
2022-05-22 01:05:49 +0800

    Use action_controller_base hook in initializer
    `action_controller` is called if the controller is an instance of `ActionController::Base` or `ActionController::API`.  And, `ActionController::API` don't have `helper_method` method.

--- lib/inertia_rails/engine.rb
+++ lib/inertia_rails/engine.rb
@@ -8,7 +8,7 @@ module InertiaRails
     end
 
     initializer "inertia_rails.action_controller" do
-      ActiveSupport.on_load(:action_controller) do
+      ActiveSupport.on_load(:action_controller_base) do
         include ::InertiaRails::Controller
       end
     end
