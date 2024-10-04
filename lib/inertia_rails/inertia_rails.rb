b6f1e64f37fbf5cc930f69d780f6e65e671b20fd
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-10-04 17:18:17 -0300

    group method for defer prop

--- lib/inertia_rails/inertia_rails.rb
+++ lib/inertia_rails/inertia_rails.rb
@@ -1,6 +1,7 @@
 # Needed for `thread_mattr_accessor`
 require 'active_support/core_ext/module/attribute_accessors_per_thread'
 require 'inertia_rails/ignore_first_load_prop'
+require 'inertia_rails/defer_prop'
 require 'inertia_rails/configuration'
 
 module InertiaRails
@@ -18,7 +19,7 @@ module InertiaRails
     InertiaRails::IgnoreFirstLoadProp.new(value, &block)
   end
 
-  def self.defer(value = nil, &block)
-    InertiaRails::IgnoreFirstLoadProp.new(value, &block)
+  def self.defer(value = nil, group = nil, &block)
+    InertiaRails::DeferProp.new(value, group, &block)
   end
 end
