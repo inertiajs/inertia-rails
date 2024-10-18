0875d68d0323a2c6a9eac41f4e9d80d70a1f9975
Svyatoslav Kryukov <me@skryukov.dev>
2024-10-18 10:41:19 +0300

    Add support for :if, :unless, :only, :except options in inertia_share

--- spec/dummy/app/controllers/inertia_conditional_sharing_controller.rb
+++ spec/dummy/app/controllers/inertia_conditional_sharing_controller.rb
@@ -7,6 +7,32 @@ class InertiaConditionalSharingController < ApplicationController
     {conditionally_shared_show_prop: 1} if action_name == "show"
   end
 
+  inertia_share only: :edit do
+    {only_block_prop: 1}
+  end
+
+  inertia_share except: [:show, :index] do
+    {except_block_prop: 1}
+  end
+
+  inertia_share if: -> { is_edit? } do
+    {if_proc_prop: 1}
+  end
+
+  inertia_share unless: -> { !is_edit? } do
+    {unless_proc_prop: 1}
+  end
+
+  inertia_share({only_prop: 1}, only: :edit)
+
+  inertia_share({if_prop: 1}, if: [:is_edit?, -> { true }])
+
+  inertia_share({unless_prop: 1}, unless: :not_edit?)
+
+  inertia_share({only_if_prop: 1}, only: :edit, if: -> { true })
+
+  inertia_share({except_if_prop: 1}, except: [:index, :show], if: -> { true })
+
   def index
     render inertia: 'EmptyTestComponent', props: {
       index_only_prop: 1,
@@ -25,9 +51,23 @@ class InertiaConditionalSharingController < ApplicationController
     }
   end
 
+  def edit
+    render inertia: 'EmptyTestComponent', props: {
+      edit_only_prop: 1,
+    }
+  end
+
   protected
 
   def conditionally_share_a_prop
     self.class.inertia_share incorrectly_conditionally_shared_prop: 1
+  end
+
+  def not_edit?
+    !is_edit?
+  end
+
+  def is_edit?
+    action_name == "edit"
   end
 end
--- spec/inertia/conditional_sharing_spec.rb
+++ spec/inertia/conditional_sharing_spec.rb
@@ -14,6 +14,20 @@ RSpec.describe "conditionally shared data in a controller", type: :request do
       expect(JSON.parse(response.body)['props'].deep_symbolize_keys).not_to include({
         conditionally_shared_show_prop: 1,
       })
+      get conditional_share_edit_path, headers: { 'X-Inertia' => true }
+      expect(JSON.parse(response.body)['props'].deep_symbolize_keys).to eq({
+        normal_shared_prop: 1,
+        only_block_prop: 1,
+        except_block_prop: 1,
+        if_proc_prop: 1,
+        unless_proc_prop: 1,
+        only_prop: 1,
+        if_prop: 1,
+        unless_prop: 1,
+        only_if_prop: 1,
+        except_if_prop: 1,
+        edit_only_prop: 1,
+      })
     end
   end
 
