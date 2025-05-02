aec6b0b3093c4812ae920a034cc5e0eb33f794b7
Brian Knoles <bknoles@gmail.com>
2025-05-01 19:10:41 -0500

    Instead of overloading the content key, use an inner_content for what goes inside a non self-closing tag

--- spec/dummy/app/controllers/inertia_meta_controller.rb
+++ spec/dummy/app/controllers/inertia_meta_controller.rb
@@ -2,7 +2,7 @@ class InertiaMetaController < ApplicationController
   def basic
     render inertia: 'TestComponent', meta: [
       { name: 'description', content: 'Inertia rules', head_key: 'first_head_key' },
-      { tag_name: 'title', content: 'The Inertia title', head_key: 'second_head_key' },
+      { tag_name: 'title', inner_content: 'The Inertia title', head_key: 'second_head_key' },
       { http_equiv: 'content-security-policy', content: "default-src 'self';", head_key: 'third_head_key'},
     ]
   end
