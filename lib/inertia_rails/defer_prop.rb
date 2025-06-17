6ee52a219e89acab6a02af759af518eefd7e9c83
Svyatoslav Kryukov <me@skryukov.dev>
2025-06-17 18:36:13 +0300

    Add match_on support

--- lib/inertia_rails/defer_prop.rb
+++ lib/inertia_rails/defer_prop.rb
@@ -4,9 +4,9 @@ module InertiaRails
   class DeferProp < IgnoreOnFirstLoadProp
     DEFAULT_GROUP = 'default'
 
-    attr_reader :group
+    attr_reader :group, :match_on
 
-    def initialize(group: nil, merge: nil, deep_merge: nil, &block)
+    def initialize(group: nil, merge: nil, deep_merge: nil, match_on: nil, &block)
       raise ArgumentError, 'Cannot set both `deep_merge` and `merge` to true' if deep_merge && merge
 
       super(&block)
@@ -14,6 +14,7 @@ module InertiaRails
       @group = group || DEFAULT_GROUP
       @merge = merge || deep_merge
       @deep_merge = deep_merge
+      @match_on = match_on.nil? ? nil : Array(match_on)
     end
 
     def merge?
--- lib/inertia_rails/merge_prop.rb
+++ lib/inertia_rails/merge_prop.rb
@@ -2,9 +2,12 @@
 
 module InertiaRails
   class MergeProp < BaseProp
-    def initialize(deep_merge: false, &block)
+    attr_reader :match_on
+
+    def initialize(deep_merge: false, match_on: nil, &block)
       super(&block)
       @deep_merge = deep_merge
+      @match_on = match_on.nil? ? nil : Array(match_on)
     end
 
     def merge?
