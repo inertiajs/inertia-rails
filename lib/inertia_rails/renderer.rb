0d81fb00e63f2fd4e0f5d4f8d9077b394ec9af5f
Svyatoslav Kryukov <me@skryukov.dev>
2026-03-04 23:24:46 +0300

    Expose shared prop keys in page response

--- lib/inertia_rails/renderer.rb
+++ lib/inertia_rails/renderer.rb
@@ -36,7 +36,9 @@ module InertiaRails
       deep_merge = options.fetch(:deep_merge, @configuration.deep_merge_shared_data)
       passed_props = options.fetch(:props,
                                    component.is_a?(Hash) ? component : @controller.__send__(:inertia_view_assigns))
-      @props = merge_props(shared_data, passed_props, deep_merge)
+      shared = shared_data
+      @shared_keys = @configuration.expose_shared_prop_keys ? extract_shared_keys(shared) : nil
+      @props = merge_props(shared, passed_props, deep_merge)
 
       @component = resolve_component(component)
 
@@ -96,6 +98,10 @@ module InertiaRails
       end
     end
 
+    def extract_shared_keys(shared_props)
+      shared_props.keys.map { |key| key.to_s.split('.', 2).first }.uniq
+    end
+
     def page
       return @page if defined?(@page)
 
@@ -132,6 +138,7 @@ module InertiaRails
       flash_data = @controller.__send__(:inertia_collect_flash_data)
       @page[:flash] = flash_data if flash_data.present?
 
+      @page[:sharedProps] = @shared_keys if @shared_keys&.any?
       @page[:preserveFragment] = @preserve_fragment if @preserve_fragment
 
       @page.merge!(metadata)
