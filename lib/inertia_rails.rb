08a72d86405c629116ae4d4145514c7eed5c2732
Svyatoslav Kryukov <me@skryukov.dev>
2026-03-05 13:19:37 +0300

    Move errors

--- lib/inertia_rails.rb
+++ lib/inertia_rails.rb
@@ -21,38 +21,6 @@ ActionController::Renderers.add :inertia do |component, options|
 end
 
 module InertiaRails
-  class Error < StandardError; end
-
-  class SSRError < Error
-    attr_reader :type, :hint, :browser_api, :stack, :source_location
-
-    def initialize(message = nil, type: nil, hint: nil, browser_api: nil, stack: nil, source_location: nil)
-      @type = type
-      @hint = hint
-      @browser_api = browser_api
-      @stack = stack
-      @source_location = source_location
-      super(message)
-    end
-
-    def self.from_response(body)
-      new(
-        body['error'] || 'Unknown SSR error',
-        type: body['type'],
-        hint: body['hint'],
-        browser_api: body['browserApi'],
-        stack: body['stack'],
-        source_location: body['sourceLocation']
-      )
-    end
-
-    def self.from_exception(exception)
-      error = new(exception.message, type: 'connection')
-      error.set_backtrace(exception.backtrace)
-      error
-    end
-  end
-
   def self.deprecator # :nodoc:
     @deprecator ||= ActiveSupport::Deprecation.new
   end
--- lib/inertia_rails/errors.rb
+++ lib/inertia_rails/errors.rb
@@ -8,4 +8,34 @@ module InertiaRails
       super('You can only call precognition once per action, use a form object to validate multiple models.')
     end
   end
+
+  class SSRError < Error
+    attr_reader :type, :hint, :browser_api, :stack, :source_location
+
+    def initialize(message = nil, type: nil, hint: nil, browser_api: nil, stack: nil, source_location: nil)
+      @type = type
+      @hint = hint
+      @browser_api = browser_api
+      @stack = stack
+      @source_location = source_location
+      super(message)
+    end
+
+    def self.from_response(body)
+      new(
+        body['error'] || 'Unknown SSR error',
+        type: body['type'],
+        hint: body['hint'],
+        browser_api: body['browserApi'],
+        stack: body['stack'],
+        source_location: body['sourceLocation']
+      )
+    end
+
+    def self.from_exception(exception)
+      error = new(exception.message, type: 'connection')
+      error.set_backtrace(exception.backtrace)
+      error
+    end
+  end
 end
